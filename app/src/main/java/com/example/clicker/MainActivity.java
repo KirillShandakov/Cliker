948cc7879424e39edf68d3b9e35cb80f1c59c0b4
KirillShandakov <asdfrut07@gmail.com>
2021-09-08 20:12:51 +0300

    init commit

---
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.clicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnAddCakes(View view) {
+        counter++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+
+    }
+
+}
